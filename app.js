dbd3f82d0ada81ed87a42cb58ae108d77d9cbedd
aathirav06 <aathiravarma06@gmail.com>
2022-07-15 02:51:14 +0530

    avatar position

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function main() {
 
   const loader1 = new THREE.GLTFLoader();
 
+  //background
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.z = 1;
   const renderer = new THREE.WebGLRenderer({ canvas });
@@ -35,18 +36,26 @@ function main() {
   );
   // scene.add(boxMesh)
 
-  loader1.load(
+  //avatar
+  const avatar = loader1.load(
     "resources/avatar.glb",
 
     function (glb) {
       console.log(glb);
       const root = glb.scene;
       scene.add(root);
-    },
-    function (error) {
-      console.log("error occured");
+      root.position.set(0,-2,0);
     }
+    
   );
+
+  // scene.add(avatar);
+  // avatar.position.set(100,100,100);
+
+  //light in avatar
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(2, 2, 5);
+  scene.add(light);
 
   function render() {
     renderer.render(scene, camera);
@@ -54,5 +63,25 @@ function main() {
   }
   requestAnimationFrame(render);
 }
+
+function ambientSetup() {
+  var ambientLight = new THREE.AmbientLight(0xcccccc, 0.8);
+  scene.add(ambientLight);
+  var pointLight = new THREE.PointLight(0xFFC5CB, .8);
+  pointLight.position.copy(camera.position);
+  scene.add(pointLight);
+}
+
+//responsive
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
+
 
 main();
